a858ec67fb45e9bf5c403aae1db6f322e31c12ae
Amin Karbas <mkarbasf@ucsc.edu>
2023-04-25 17:33:09 -0700

    Nit: simplify post_process.py

--- labs/LAB2/post_process.py
+++ labs/LAB2/post_process.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import csv
@@ -5,45 +7,34 @@ import csv
 
 # Identify desired path
 path = sys.argv[1]
+files = os.listdir(path)
+metrics = [x.lower() for x in sys.argv[2:]]
+csv_path = os.path.join(path, 'out.csv')
+res = {}
 
-arg_size = len(sys.argv)
-entries_list=[]
-
-# Get passed entries
-for i in range(2,arg_size):
-    entries_list.append(sys.argv[i])
-
-# Prep CSV output
-csv_output = csv.writer(open('csv_output.csv', 'w'))
+csv_output = csv.writer(open(csv_path, 'w'))
 csv_output.writerow(['entry','total','ratio'])
 
-ipc_counter = 0
+for f in files:
+    if (os.path.isdir(f)
+        or not f.endswith('.out')
+        or not '.stat.' in f):
+        continue
 
-files = os.listdir(path)
-# Search for the listed entries
-for file in files:
-    if os.path.isdir(file):
-        continue
-    if not file.endswith(".out"):
-        continue
-    text_file = open(file,"r")
-    lines = text_file.readlines()
-    text_file.close()
-    for line in lines:
-        #Extra check for IPC
-        if "IPC:" in line and ipc_counter != 1:
-            text = line.split()
-            ipc_index = text.index('IPC:')
-            entry = text[ipc_index]
-            total = "n/a"
-            ratio = text[ipc_index + 1]
-            csv_output.writerow([entry, total, ratio])
-            ipc_counter += 1
-            continue
-        check = any(item in entries_list for item in line.split())
-        if check is True:
-            text = line.split()
-            entry = text[0]
-            total = text[1]
-            ratio = text[2]
-            csv_output.writerow([entry, total, ratio])
+    with open(os.path.join(path, f), 'rt') as stat_file:
+        for line in stat_file:
+            line_parts = line.strip().lower().split()
+            if 'ipc:' in line_parts and 'ipc' in metrics:
+                ratio = line_parts[line_parts.index('ipc:') + 1]
+                res['ipc'] = ('n/a', ratio)
+            if any(x in metrics for x in line_parts):
+                metric, total, ratio = line_parts[:3]
+                res[metric] = (total, ratio)
+
+with open(csv_path, 'wt') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['entry', 'tota', 'ratio'])
+    for metric, vals in res.items():
+        print(metric, vals)
+        csv_writer.writerow([metric, *vals])
+
