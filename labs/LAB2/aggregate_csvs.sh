b5c6393171a7402a5aab03a901ddf1d8f3ee91da
Amin Karbas <mkarbasf@ucsc.edu>
2023-04-25 18:38:08 -0700

    Fix aggregate csv

--- labs/LAB2/aggregate_csvs.sh
+++ labs/LAB2/aggregate_csvs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eEuo pipefail
 
-cat runs/config*/*/out.csv | grep metric | sort | uniq | sed 's/^/run,/g' > aggregate.csv
+cat runs/config*/*/out.csv | grep metric | sort | uniq | sed 's/^/run,config,/g' > aggregate.csv
 
 for batch in $(ls runs); do
   batch_dir=runs/${batch}
   for run in $(ls ${batch_dir}); do
     run_dir=${batch_dir}/${run}
-    cat ${run_dir}/out.csv | grep -v metric | sed "s/^/${run}-${batch},/g" >> aggregate.csv
+    cat ${run_dir}/out.csv | grep -v metric | sed "s/^/${run},${batch},/g" >> aggregate.csv
   done
 done
