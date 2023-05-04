b2c266e73c993e666e745fd4d9819f2619eb1819
Amin Karbas <mkarbasf@ucsc.edu>
2023-05-04 15:30:34 -0700

    Add post-procs

--- labs/LAB3/aggregate_csvs.sh
+++ labs/LAB3/aggregate_csvs.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -eEuo pipefail
 
-cat runs/config*/*/out.csv | grep metric | sort | uniq | sed 's/^/run,config,/g' > aggregate.csv
+cat runs/*/out.csv | grep metric | sort | uniq | sed 's/^/run,/g' > aggregate.csv
 
-for batch in $(ls runs); do
-  batch_dir=runs/${batch}
-  for run in $(ls ${batch_dir}); do
-    run_dir=${batch_dir}/${run}
-    cat ${run_dir}/out.csv | grep -v metric | sed "s/^/${run},${batch},/g" >> aggregate.csv
-  done
+for run in $(ls runs/); do
+  run_dir=runs/${run}
+  cat ${run_dir}/out.csv | grep -v metric | sed "s/^/${run},/g" >> aggregate.csv
 done
--- labs/LAB3/run_post_procs.sh
+++ labs/LAB3/run_post_procs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eEuxo pipefail
 
-for x in runs/*/*; do ./post_process.py ${x} dcache_miss ipc; done
+for x in runs/*; do ./post_process.py ${x} dcache_miss dcache_miss_compulsory dcache_miss_capacity dcache_miss_conflict ipc; done
